467f9ec3327459ce9be5ca7d051e9a4de0d2e629
velichko <vel.angelov@gmail.com>
2017-06-15 12:39:28 +0300

    final

--- ITT_VA_P07/src/L_07/P13.java
+++ ITT_VA_P07/src/L_07/P13.java
@@ -1,5 +1,34 @@
 package L_07;
 
-public class P13 {
+import java.util.Scanner;
 
+public class P13 {
+	public static void main(String[] args) {
+		System.out.println("Enter n and m:");
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int m = in.nextInt();
+		int[][] arr = new int[n][m];
+		int counter = 1;
+		int j,i;
+		for (i = 0; i < m; i++) {
+			if (i % 2 == 0) {
+				for (j = 0; j < arr.length; j++) {
+					arr[j][i] = counter;
+					counter++;
+				}
+			} else {
+				for (j = arr.length - 1; j >= 0; j--) {
+					arr[j][i] = counter;
+					counter++;
+				}
+			}
+		}
+		System.out.println();
+		for (i = 0; i < arr.length; i++) {
+			for (j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}	}
 }
